7a949b36fa3af732eb40f38083e3478a04a0745e
BbtoAlcrz <benito11kpo@gmail.com>
2026-08-16 22:31:58 -0300

    añadí al practico2

---
+++ Practico2/Practico2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Practico2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LNyA = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LDni = new System.Windows.Forms.Label();
+            this.LModificar = new System.Windows.Forms.Label();
+            this.TDni = new System.Windows.Forms.TextBox();
+            this.TNombre = new System.Windows.Forms.TextBox();
+            this.TApellido = new System.Windows.Forms.TextBox();
+            this.BGuardar = new System.Windows.Forms.Button();
+            this.BEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LNyA
+            // 
+            this.LNyA.AutoSize = true;
+            this.LNyA.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LNyA.Location = new System.Drawing.Point(38, 45);
+            this.LNyA.Name = "LNyA";
+            this.LNyA.Size = new System.Drawing.Size(140, 16);
+            this.LNyA.TabIndex = 0;
+            this.LNyA.Text = "Nombre y Apellido:";
+            this.LNyA.Click += new System.EventHandler(this.LNyA_Click);
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LNombre.Location = new System.Drawing.Point(38, 139);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(56, 16);
+            this.LNombre.TabIndex = 1;
+            this.LNombre.Text = "Nombre";
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LApellido.Location = new System.Drawing.Point(38, 182);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(57, 16);
+            this.LApellido.TabIndex = 2;
+            this.LApellido.Text = "Apellido";
+            // 
+            // LDni
+            // 
+            this.LDni.AutoSize = true;
+            this.LDni.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LDni.Location = new System.Drawing.Point(39, 101);
+            this.LDni.Name = "LDni";
+            this.LDni.Size = new System.Drawing.Size(30, 16);
+            this.LDni.TabIndex = 3;
+            this.LDni.Text = "DNI";
+            this.LDni.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LModificar
+            // 
+            this.LModificar.AutoSize = true;
+            this.LModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LModificar.ForeColor = System.Drawing.Color.Firebrick;
+            this.LModificar.Location = new System.Drawing.Point(219, 45);
+            this.LModificar.Name = "LModificar";
+            this.LModificar.Size = new System.Drawing.Size(71, 16);
+            this.LModificar.TabIndex = 4;
+            this.LModificar.Text = "Modificar";
+            this.LModificar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // TDni
+            // 
+            this.TDni.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TDni.Location = new System.Drawing.Point(126, 98);
+            this.TDni.Name = "TDni";
+            this.TDni.Size = new System.Drawing.Size(171, 22);
+            this.TDni.TabIndex = 1;
+            // 
+            // TNombre
+            // 
+            this.TNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TNombre.Location = new System.Drawing.Point(126, 136);
+            this.TNombre.Name = "TNombre";
+            this.TNombre.Size = new System.Drawing.Size(171, 22);
+            this.TNombre.TabIndex = 2;
+            // 
+            // TApellido
+            // 
+            this.TApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TApellido.Location = new System.Drawing.Point(126, 179);
+            this.TApellido.Name = "TApellido";
+            this.TApellido.Size = new System.Drawing.Size(171, 22);
+            this.TApellido.TabIndex = 3;
+            // 
+            // BGuardar
+            // 
+            this.BGuardar.Location = new System.Drawing.Point(222, 221);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(75, 23);
+            this.BGuardar.TabIndex = 4;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.UseVisualStyleBackColor = true;
+            // 
+            // BEliminar
+            // 
+            this.BEliminar.Location = new System.Drawing.Point(126, 221);
+            this.BEliminar.Name = "BEliminar";
+            this.BEliminar.Size = new System.Drawing.Size(75, 23);
+            this.BEliminar.TabIndex = 5;
+            this.BEliminar.Text = "Eliminar";
+            this.BEliminar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Application;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 290);
+            this.Controls.Add(this.BEliminar);
+            this.Controls.Add(this.BGuardar);
+            this.Controls.Add(this.TApellido);
+            this.Controls.Add(this.TNombre);
+            this.Controls.Add(this.TDni);
+            this.Controls.Add(this.LModificar);
+            this.Controls.Add(this.LDni);
+            this.Controls.Add(this.LApellido);
+            this.Controls.Add(this.LNombre);
+            this.Controls.Add(this.LNyA);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pequeño Formulario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LNyA;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LDni;
+        private System.Windows.Forms.Label LModificar;
+        private System.Windows.Forms.TextBox TDni;
+        private System.Windows.Forms.TextBox TNombre;
+        private System.Windows.Forms.TextBox TApellido;
+        private System.Windows.Forms.Button BGuardar;
+        private System.Windows.Forms.Button BEliminar;
+    }
+}
+
